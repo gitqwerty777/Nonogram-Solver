108e4c5f05888f18eec75eb3747af2e9e7233e00
gitqwerty777 <goohcl777@gmail.com>
2015-10-21 15:10:25 +0800

    refact: add limitFiller

--- dfsboard.h
+++ dfsboard.h
@@ -4,15 +4,32 @@
 #include "board.h"
 #include "unistd.h"
 #include "assert.h"
+
+class LimitFiller{
+public:
+  LimitFiller(){l.clear(); fillStart.clear();};
+  LimitFiller(vector<Limit> limits): l(limits){isInit = true;};
+  bool isInit;
+  vector<Limit> l;
+  vector<int> fillStart;
+  bool isInited(){return isInit;}
+  void setLimit(vector<Limit>& limits){l = limits; isInit = true;}
+  void destroy(){isInit = false; fillStart.clear();}
+  bool getNextFillStart();
+  bool getNextFillStartbyFillStart();
+  bool isLimitLegal();
+};
+
 class DFSBoard: public Board{
- public:
- DFSBoard(Board& b): Board(b){//inherit
-    this->printBoard("before DFS");
-    lastfillStart.resize(r);
+public:
+  DFSBoard(Board& b): Board(b){//inherit
+    printBoard("before DFS");
+    limitFillers.resize(r);
     backupBoards.resize(r);
   }
 
-  vector< vector<int> > lastfillStart;
+  vector< LimitFiller > limitFillers;
+  vector <vector<int> > lastfillStart;
   vector<Board> backupBoards;
 
   void Restore(int);
